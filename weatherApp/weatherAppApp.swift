9ac309f23df2ca0c5800daeda7636fb66ae8a3e9
vladyslav <nnnnnniikttttto@gmail.com>
2025-10-27 13:21:07 +0100

    initial commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Vladick  on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
